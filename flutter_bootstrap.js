3b76bba7fd1fe8ec46885c1043a371e3a8d13fca
Mariusz Zdziennicki <mariusz.zdziennicki@int.pl>
2025-10-02 13:49:19 +0200

    deploy - 2025-10-02 13:49:18

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2043328876"
+    serviceWorkerVersion: "1014411344"
   }
 });
